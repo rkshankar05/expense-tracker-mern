f86a5021ed3c82f6f75143eeaf1ebe49ac8a29a6
rkshankar05 <rkshankar.13138@gmail.com>
2026-05-20 02:19:03 +0530

    updated production api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-mern-ewk7.onrender.com"
+  baseURL: "https://expense-tracker-mern-ewk7.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
